7c74eb9a0d2e8071a905fdb91709509cf07b9af6
Naoya Yamashita <conao3@gmail.com>
2020-10-01 14:51:11 +0900

    generate project

---
+++ markuplint.el
@@ -0,0 +1,38 @@
+;;; markuplint.el --- Frontend for markuplint  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/markuplint.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Frontend for markuplint.
+
+
+;;; Code:
+
+(defgroup markuplint nil
+  "Frontend for markuplint."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/markuplint.el"))
+
+(provide 'markuplint)
+
+;;; markuplint.el ends here
